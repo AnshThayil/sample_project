6f7eb5c816f52534d20ff1f0308949377319dd9c
Ansh Thayil <anshthayil0114@gmail.com>
2020-06-25 11:32:20 -0700

    Implemented /users/create API route

--- backend/users/views.py
+++ backend/users/views.py
@@ -7,3 +7,11 @@ from .models import User
 def index(request):
     users = serializers.serialize("json", User.objects.all())
     return HttpResponse(users)
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        phone = request.POST["phone"]
+        new_user = User(name=name, email=email, phone=phone)
+        new_user.save()
